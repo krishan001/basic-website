e684e565651a7ff114c031d415bce93babb47e3d
Krishan Patel <sc17kdp@leeds.ac.uk>
2018-04-29 14:56:10 +0100

    added some javascript

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    alert("jquery loaded");
-});
+
+$("#choice").change(function(){
+    $("#table tbody tr").hide();
+    $("#table tbody tr."+$(this).val()).show('fast');
+  });
+
+  
