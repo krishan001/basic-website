9f060b89687d229fc8575a5c886e0077befac0d6
Krishan Patel <sc17kdp@leeds.ac.uk>
2018-04-29 16:21:00 +0100

    added calculator

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,7 +1,18 @@
 
-$("#choice").change(function(){
-    $("#table tbody tr").hide();
-    $("#table tbody tr."+$(this).val()).show('fast');
-  });
+$(document).ready(function(){
 
-  
+  $("#table tbody tr").hide();
+  $("#table tbody tr."+$("#choice").val()).show('fast');
+  $("#choice").change(function(){
+      $("#table tbody tr").hide();
+      $("#table tbody tr."+$(this).val()).show('fast');
+    });
+
+    $("#tdee").submit(function(){
+      let value = parseInt( $("#bodyweight").val());
+      value = value + 10;
+      $("#result").html(value);
+      return false;
+    });
+
+});
