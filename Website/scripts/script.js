6ad9c011b758f0ad68458eeb263fbb31c8f283fa
Krishan Patel <sc17kdp@leeds.ac.uk>
2018-04-30 12:07:12 +0100

    finished macro calculator

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -2,16 +2,21 @@
 $(document).ready(function(){
 
   $("#table tbody tr").hide();
-  $("#table tbody tr."+$("#choice").val()).show('fast');
+  $("#table tbody tr."+$("#choice").val()).show();
   $("#choice").change(function(){
       $("#table tbody tr").hide();
       $("#table tbody tr."+$(this).val()).show('fast');
     });
 
-    $("#tdee").submit(function(){
-      let value = parseInt( $("#bodyweight").val());
-      value = value + 10;
-      $("#result").html(value);
+    $("#macros").submit(function(){
+      let weight = parseInt( $("#bodyweight").val());
+      let calories = 2000;
+      let protein = weight;
+      let fat = weight * 0.3;
+      let carbs = (calories - ((protein * 4) + (fat * 9)))/4;
+      $("#protein").html( protein+ "g of protein" );
+      $("#fat").html( fat + "g of fat");
+      $("#carbs").html("<p>" + carbs + "g of carbs </p>");
       return false;
     });
 
