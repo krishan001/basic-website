e5db3aac55ec1310ac6e85fae4ad8de56f645b84
Krishan Patel <sc17kdp@leeds.ac.uk>
2018-05-03 13:02:00 +0100

    finished website

--- Website/scripts/calc.js
+++ Website/scripts/calc.js
@@ -1,30 +1,26 @@
 $(document).ready(function () {
 
-  
+
     $("#macros").submit(function () {
-      let weight = parseInt($("#bodyweight").val());
-      let calories = 2000;
-      let protein = weight;
-      let fat = weight * 0.3;
-      let carbs = (calories - ((protein * 4) + (fat * 9))) / 4;
-      $("#protein").html(protein + "g of protein");
-      $("#fat").html(fat + "g of fat");
-      $("#carbs").html("<p>" + carbs + "g of carbs </p>");
-      return false;
-    });
-    $("calcButton").click(function(){
-        $("1rm").submit();
-    });
-    $("#1rm").submit(function () {
-        let weight = parseInt($("#weight").val());
-        console.log(weight);
-        let reps = parseInt($("#reps").val());
-        console.log(reps);
-        let repMax = weight * (1 + (reps/30));
-    
-        $("#maxweight").html(repMax + " kg");
-        
+        let weight = parseInt($("#bodyweight").val());
+        let calories = 2000;
+        let protein = weight;
+        let fat = weight * 0.3;
+        let carbs = (calories - ((protein * 4) + (fat * 9))) / 4;
+        $("#protein").html(protein + "g of protein");
+        $("#fat").html(fat + "g of fat");
+        $("#carbs").html("<p>" + carbs + "g of carbs </p>");
         return false;
     });
-    
+
 });
+
+function rm() {
+
+    let weight = parseInt($("#weight").val());
+    let reps = parseInt($("#reps").val());
+    let repMax = weight * (1 + (reps / 30));
+    $("#maxweight").html(Math.floor(repMax) + " kg");
+
+    return false;
+}
